f6ea65c710144a937cff3b76dbe8cf0132cff370
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-09-12 18:06:58 +0200

    rettelse så scanner ikke printer flere spørgsmål i terminal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,34 +4,41 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-
-
         //Udprinter velkomst
         System.out.println("Velkommen til superhelte universet!!!");
 
         System.out.print("Indtast navnet på din superhelt (fx spiderman): ");
-        String superheltnavn = scanner.nextLine();
+        String superheltNavn = scanner.nextLine();
 
         System.out.print("Indtast superheltens rigtige navn (fx Peter Parker): ");
-        String rigtigenavn = scanner.nextLine();
+        String rigtigeNavn = scanner.nextLine();
 
         System.out.print("Er din superhelt et menneske (true/false): ");
         boolean erMenneske = scanner.nextBoolean();
+        scanner.nextLine();
 
         System.out.print("Indtast superheltens superpower: ");
-        String superpower = scanner.nextLine();
+        String superPower = scanner.nextLine();
+
 
         System.out.print("Indtast det år superhelten blev skabt: ");
         int skabelsesÅr = scanner.nextInt();
+        scanner.nextLine();
 
         System.out.print("Indtast superheltens styrke: ");
         double styrke = scanner.nextDouble();
 
 
+
+
+
+
+        }
+
     }
 
 
 
 
-    }
+
 
