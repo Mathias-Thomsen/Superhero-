6eace9273d24ed8471f879372b25706d13445889
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-09-14 08:18:39 +0200

    commit test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,25 +22,26 @@ public class Main {
             scanner.nextLine();
 
             if (menuValg == 1) {
-                System.out.print("Indtast navnet på din superhelt (fx spiderman): ");
+                System.out.print("Indtast navnet på din superhelt (fx Spiderman): ");
                 String superHeroName = scanner.nextLine();
                 System.out.print("Indtast superheltens rigtige navn (fx Peter Parker): ");
                 String reelName = scanner.nextLine();
                 System.out.print("Er din superhelt et menneske (true/false): ");
                 boolean isHuman = scanner.nextBoolean();
                 scanner.nextLine();
-                System.out.print("Indtast superheltens superpower: ");
+                System.out.print("Indtast superheltens superpower (fx stærk): ");
                 String superPower = scanner.nextLine();
-                System.out.print("Indtast det år superhelten blev skabt: ");
+                System.out.print("Indtast det år superhelten blev skabt (fx 1945): ");
                 int creationYear = scanner.nextInt();
                 scanner.nextLine();
-                System.out.print("Indtast superheltens styrke: ");
+                System.out.print("Indtast superheltens styrke (med ',' f.eks. 1,5): ");
                 double styrke = scanner.nextDouble();
                 database.createSuperHero(superHeroName, reelName, isHuman, superPower, creationYear, styrke);
 
             } else if (menuValg == 9) {
                 System.out.println("Programmet afsluttes");
             }else if (menuValg == 2) {
+                System.out.println("Liste af superhelte: ");
                 database.showListOfSuperheroes();
 
             }
