da0dd5e044a8497aaa9adb96a9d0f4d478c21eda
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-09-20 21:49:08 +0200

    testdatabase

---
+++ src/test/java/DatabaseTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import javax.xml.crypto.Data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+    @Test
+
+    public Database testCreateSuperhero() {
+        //Arrange
+        Database database = new Database();
+        Database exspected = database.createSuperHero();
+
+        //Act
+
+        //Assert
+    }
+
+}
